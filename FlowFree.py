f5006302382919f81c42885ea3519e0fe62a4d9d
Sophia Aristizabal Florez <67614769+sophiearisti@users.noreply.github.com>
2025-11-15 11:09:19 -0500

    created the basic classes for the project

---
+++ FlowFree.py
@@ -0,0 +1,48 @@
+## =========================================================================
+## @authors Isaac Janica, Sophia Aristizabal
+## =========================================================================
+
+## =========================================================================
+## MANUAL DE USO 
+## Este es el archivo principal que ejecuta el juego de buscaminas.
+## Se debe ejecutar desde la terminal de la siguiente manera:
+## python3 MineSweeper.py width height mines player <arguments>
+## donde:
+## - width: ancho del tablero (numero de columnas)
+## - height: alto del tablero (numero de filas) 
+## - mines: numero de minas
+## - player: archivo del jugador (por ejemplo, code/Player/Random.py)
+
+## Para poder utilizar el codigo, es necesario tener instaladas las siguientes
+## librerias de Python:
+## - pandas
+## - numpy
+## - scikit-learn
+## =========================================================================
+
+
+import importlib.util, sys
+from Board import *
+from Solver import *
+
+if __name__ == "__main__":
+    # Lectura de argumentos
+    if len(sys.argv) < 2:
+        print("Usage: python3  FlowFree.py <filename>.txt")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    
+    # Crear el tablero
+    board = Board(filename)
+    
+    # Crear el solver
+    
+    solver = Solver(board)
+    
+    # Resolver el tablero
+    if solver.solve():
+        print("Solucionado!")
+        board.print_board()
+    else:
+        print("No se encontró solución.")
